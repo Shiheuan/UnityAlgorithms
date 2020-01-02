c3458c12a3f56018c5f883029556090a51793c2e
shiheuan <shiheuan@gmail.com>
2020-01-02 12:59:34 +0800

    Insertion sort.

---
+++ Assets/Source/SortingAlgorithm/2_Insertion/Insertion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public class Insertion : BaseSort
+    {
+        public static void sort(IComparable[] a)
+        {
+            int N = a.Length;
+            for (var i = 1; i < N; i++)
+            {
+                for (var j = i; j > 0 && less(a[j], a[j - 1]); j--)
+                {
+                    // exchange every step.
+                    exch(a, j, j-1);
+                }
+            }
+        }
+    }
+}
+
