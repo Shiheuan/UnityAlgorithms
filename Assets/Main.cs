d8783bae8a29024aebc06c910ab66332a8b69995
shiheuan <shiheuan@gmail.com>
2020-01-02 15:13:40 +0800

    compare Selection and Insertion sorting time.

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms.Foundations;
+using Algorithms.Sorting;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -6,14 +7,19 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var bag = new Bag<object>();
-        bag.add(new object());
-        bag.add(new object());
-        bag.add(new object());
-        foreach (var v in bag)
-        {
-            print(v);
-        }
+        string alg1 = "Selection";
+        string alg2 = "Insertion";
+
+        int N = 1000;
+        int T = 100;
+
+        double t1 = SortCompare.timeRandomInput(alg1, N, T);
+        double t2 = SortCompare.timeRandomInput(alg2, N, T);
+        print(t1);
+        print(t2);
+
+        print($"For {N} random Doubles\n");
+        print($"{alg2} is {t1/t2} times faster than {alg1}\n");
     }
 
     // Update is called once per frame
---
+++ Assets/Source/SortingAlgorithm/SortCompare.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Algorithms.Sorting
+{
+    public class SortCompare
+    {
+        public static double time(string alg, IComparable[] a)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            switch (alg)
+            {
+                case "Insertion":
+                    Insertion.sort(a);
+                    break;
+                case "Selection":
+                    Selection.sort(a);
+                    break;
+                case "Shell":
+                    throw new NotImplementedException();
+                case "Merge":
+                    throw new NotImplementedException();
+                case "Quick":
+                    throw new NotImplementedException();
+                case "Heap":
+                    throw new NotImplementedException();
+            }
+            timer.Stop();
+            return timer.ElapsedMilliseconds;
+        }
+
+        public static double timeRandomInput(string alg, int N, int T)
+        {
+            // use alg sort T arrays which have N elements.
+            double total = 0.0;
+            IComparable[] a = new IComparable[N];
+            var rnd = new Random();
+            for (int t = 0; t < T; t++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    a[i] = rnd.NextDouble();
+                }
+
+                total += time(alg, a);
+            }
+
+            return total;
+        }
+    }
+}
+
