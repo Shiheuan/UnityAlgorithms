30d64eb6c653f912d8914976409d1d575715a1a6
shiheuan <shiheuan@gmail.com>
2020-01-02 17:50:04 +0800

    Merge sort.

---
+++ Assets/Source/SortingAlgorithm/4_Merge/Editor/TestMerge.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Sorting
+{
+    public class TestMerge
+    {
+        [Test]
+        public void sort_InputStringArray_AscendingOrder()
+        {
+            string[] s = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
+            Merge.sort(s);
+            Assert.True(BaseSort.isSorted(s));
+        }
+    }
+}
+
---
+++ Assets/Source/SortingAlgorithm/4_Merge/Merge.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public class Merge : BaseSort
+    {
+        private static IComparable[] aux;
+
+        public static void sort(IComparable[] a)
+        {
+            aux = new IComparable[a.Length];
+            sort(a, 0, a.Length - 1);
+        }
+
+        private static void sort(IComparable[] a, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int mid = lo + (hi - lo) / 2;
+            sort(a, lo, mid);
+            sort(a, mid + 1, hi);
+            merge(a, lo, mid, hi);
+        }
+
+        private static void merge(IComparable[] a, int lo, int mid, int hi)
+        {
+            int i = lo, j = mid + 1;
+
+            for (int k = lo; k <= hi; k++)
+            {
+                aux[k] = a[k];
+            }
+
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid) a[k] = aux[j++];
+                else if (j > hi) a[k] = aux[i++];
+                else if (less(aux[i], aux[j])) a[k] = aux[i++];
+                else a[k] = aux[j++];
+            }
+        }
+    }
+}
+
