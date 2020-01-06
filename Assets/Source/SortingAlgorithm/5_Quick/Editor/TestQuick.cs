3f43caae4364c2d4821d9ef0c1e0172a2db4c1a8
shiheuan <shiheuan@gmail.com>
2020-01-06 11:43:32 +0800

    3 Way Quick Sort.

--- Assets/Source/SortingAlgorithm/5_Quick/Editor/TestQuick.cs
+++ Assets/Source/SortingAlgorithm/5_Quick/Editor/TestQuick.cs
@@ -11,6 +11,14 @@ namespace Algorithms.Sorting
             Quick.sort(s);
             Assert.True(BaseSort.isSorted(s));
         }
+
+        [Test]
+        public void sort3way_InputStringArray_AscendingOrder()
+        {
+            string[] s = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
+            Quick3Way.sort(s);
+            Assert.True(BaseSort.isSorted(s));
+        }
     }
 
 }
---
+++ Assets/Source/SortingAlgorithm/5_Quick/Quick3Way.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public class Quick3Way : BaseSort
+    {
+        public static void sort(IComparable[] a)
+        {
+            shuffle(a);
+            sort(a, 0, a.Length - 1);
+        }
+
+        private static void shuffle(IComparable[] a)
+        {
+
+        }
+
+        public static void sort(IComparable[] a, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int lt = lo, i = lo + 1, gt = hi;
+            IComparable v = a[lo];
+            while (i <= gt)
+            {
+                int cmp = a[i].CompareTo(v);
+                if (cmp < 0) exch(a, lt++, i++);
+                else if (cmp > 0) exch(a, i, gt--);
+                else i++;
+            }
+            sort(a, lo, lt-1);
+            sort(a, gt + 1, hi);
+        }
+    }
+}
+
