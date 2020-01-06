b6f4591e75179441be84627729b11076a55635f3
shiheuan <shiheuan@gmail.com>
2020-01-06 17:46:34 +0800

    Priority Queue with Binary Heap.

---
+++ Assets/Source/SortingAlgorithm/PriorityQueue/MaxPQ.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using UnityScript.Macros;
+
+namespace Algorithms.Sorting
+{
+    public class MaxPQ<T> where T : IComparable<T>
+    {
+        private T[] pq;
+        private int N;
+
+        public MaxPQ(int maxN)
+        {
+            pq = new T[maxN];
+        }
+
+        public bool isEmpty()
+        {
+            return N == 0;
+        }
+
+        public int size()
+        {
+            return N;
+        }
+
+        public void insert(T v)
+        {
+            pq[++N] = v;
+            swim(N);
+        }
+
+        public T delMax()
+        {
+            T max = pq[1];
+            exch(1, N--);
+            pq[N + 1] = default(T); // null
+            sink(1);
+            return max;
+        }
+
+        private bool less(int i, int j)
+        {
+            return pq[i].CompareTo(pq[j]) < 0;
+        }
+
+        private void exch(int i, int j)
+        {
+            T t = pq[i];
+            pq[i] = pq[j];
+            pq[j] = t;
+        }
+
+        private void swim(int k)
+        {
+            while (k > 1 && less(k / 2, k))
+            {
+                exch(k, k/2);
+                k = k / 2;
+            }
+        }
+
+        private void sink(int k)
+        {
+            while (2 * k <= N)
+            {
+                int j = 2 * k;
+                if (j < N && less(j, j + 1))
+                {
+                    j++;
+                }
+
+                if (!less(j, k))
+                {
+                    break;// end
+                }
+                exch(j, k);
+                k = j;
+            }
+        }
+    }
+
+}
+
