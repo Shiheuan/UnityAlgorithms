6bc0016a925127b5f2bf128bfc9845e6f81fec1c
Shiheuan <shiheuan@gmail.com>
2020-02-25 02:27:17 +0800

    Hash Table.

---
+++ Assets/Source/SearchAlgorithm/6_HashTable/LinearProbingHashST.cs
@@ -0,0 +1,107 @@
+﻿namespace Algorithms.Search
+{
+    public class LinearProbingHashST<TKey, TValue>
+    {
+        private int N;
+        private int M = 16;
+        private TKey[] keys;
+        private TValue[] vals;
+
+        public LinearProbingHashST()
+        {
+            keys = new TKey[M];
+            vals = new TValue[M];
+        }
+
+        public LinearProbingHashST(int cap)
+        {
+            keys = new TKey[cap];
+            vals = new TValue[cap];
+            M = cap;
+        }
+
+        private int hash(TKey key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % M;
+        }
+
+        private void resize(int cap)
+        {
+            LinearProbingHashST<TKey, TValue> t;
+            t = new LinearProbingHashST<TKey, TValue>(cap);
+            for (int i = 0; i < M; i++)
+            {
+                if(keys[i] != null)
+                {
+                    t.put(keys[i], vals[i]);
+                }
+            }
+
+            keys = t.keys;
+            vals = t.vals;
+            M = t.M;
+        }
+
+        public void put(TKey key, TValue val)
+        {
+            if (N >= M / 2) resize(2 * M);
+            int i;
+            for (i = hash(key); keys[i] != null; i = (i + 1) % M)
+            {
+                if (keys[i].Equals(key))
+                {
+                    vals[i] = val;
+                    return;
+                }
+            }
+
+            keys[i] = key;
+            vals[i] = val;
+            N++;
+        }
+
+        public TValue get(TKey key)
+        {
+            for (int i = hash(key); keys[i] != null; i = (i + 1) % M)
+            {
+                if (keys[i].Equals(key))
+                    return vals[i];
+            }
+
+            return default(TValue);
+        }
+
+        public bool contains(TKey key)
+        {
+            return get(key) != null;
+        }
+
+        public void delete(TKey key)
+        {
+            if (!contains(key)) return;
+            int i = hash(key);
+            while (!key.Equals(keys[i]))
+            {
+                i = (i + 1) % M;
+            }
+
+            keys[i] = default(TKey);//null;
+            vals[i] = default(TValue); //null;
+            i = (i + 1) % M;
+            while (keys[i] != null)
+            {
+                TKey keyToRedo = keys[i];
+                TValue valToRedo = vals[i];
+                keys[i] = default(TKey);//null;
+                vals[i] = default(TValue); //null;
+                N--;
+                put(keyToRedo, valToRedo);
+                i = (i + 1) % M;
+            }
+
+            N--;
+            if (N > 0 && N == M/8) resize(M/2);
+        }
+    }
+}
+
---
+++ Assets/Source/SearchAlgorithm/6_HashTable/SeparateChainingHashST.cs
@@ -0,0 +1,37 @@
+﻿namespace Algorithms.Search
+{
+    public class SeparateChainingHashST<TKey, TValue>
+    {
+        private int N; // 键值对总数
+        private int M; // 散列表的大小
+        private SequentialSearchST<TKey, TValue>[] st; // 存放链表对象的数组
+
+        public SeparateChainingHashST() : this(997) { }
+
+        public SeparateChainingHashST(int M)
+        {
+            this.M = M;
+            st = new SequentialSearchST<TKey, TValue>[M];
+            for (int i = 0; i < M; i++)
+            {
+                st[i] = new SequentialSearchST<TKey, TValue>();
+            }
+        }
+
+        private int hash(TKey key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % M;
+        }
+
+        public TValue get(TKey key)
+        {
+            return (TValue) st[hash(key)].get(key);
+        }
+
+        public void put(TKey key, TValue val)
+        {
+            st[hash(key)].put(key, val);
+        }
+    }
+}
+
