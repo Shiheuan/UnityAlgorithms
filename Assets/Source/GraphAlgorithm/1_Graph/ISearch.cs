0a4fe292628f74bdf4d5ac1d689c183e7805ca55
Shiheuan <shiheuan@gmail.com>
2020-02-25 23:42:37 +0800

    Graph Search interface and Depth First Search Algorithm.

---
+++ Assets/Source/GraphAlgorithm/1_Graph/ISearch.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Graph
+{
+    public interface ISearch
+    {
+        bool marked(int v); // s 与 v 是否连通
+
+        int count(); // 与 s 连通的顶点总数
+    }
+}
+
---
+++ Assets/Source/GraphAlgorithm/2_DepthFirstSearch/DepthFirstSearch.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms.Graph
+{
+    public class DepthFirstSearch : ISearch
+    {
+        private bool[] mark;
+        private int N;
+
+        public DepthFirstSearch(Graph G, int s)
+        {
+            mark = new bool[G.v()];
+            dfs(G, s);
+        }
+
+        private void dfs(Graph G, int v)
+        {
+            mark[v] = true;
+            N++;
+            foreach (int w in G.adj(v))
+            {
+                if (!mark[w]) dfs(G, w);
+            }
+        }
+
+        public bool marked(int v)
+        {
+            return mark[v];
+        }
+
+        public int count()
+        {
+            return N;
+        }
+    }
+}
+
