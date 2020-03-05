c182974f86ca2f392367419e0abdfcac70cd6c21
Shiheuan <shiheuan@gmail.com>
2020-03-05 17:03:17 +0800

    Check Digraph with Directed Cycle.

---
+++ Assets/Source/GraphAlgorithm/8_DirectedCycle/DirectedCycle.cs
@@ -0,0 +1,60 @@
+﻿using Algorithms.Foundations;
+
+namespace Algorithms.Graph
+{
+    public class DirectedCycle
+    {
+        private bool[] mark;
+        private int[] edgeTo;
+        private Stack<object> cycle;    // vertex in cycle.
+        private bool[] onStack;         // vertex on recursive call stack
+
+        public DirectedCycle(Digraph G)
+        {
+            onStack = new bool[G.v()];
+            edgeTo = new int[G.v()];
+            mark = new bool[G.v()];
+            for (int v = 0; v < G.v(); v++)
+            {
+                if (!mark[v])
+                    dfs(G, v);
+            }
+        }
+
+        private void dfs(Digraph G, int v)
+        {
+            onStack[v] = true;
+            mark[v] = true;
+            foreach (int w in G.adj(v))
+            {
+                if (this.hasCycle()) return;
+                else if (!mark[w])
+                {
+                    edgeTo[w] = v;
+                    dfs(G, w);
+                }
+                else if (onStack[w])
+                {
+                    cycle = new Stack<object>();
+                    for (int x = v; x != w; x = edgeTo[x])
+                    {
+                        cycle.push(x);
+                    }
+                    cycle.push(w);
+                    cycle.push(v);
+                }
+            }
+            onStack[v] = false;
+        }
+
+        public bool hasCycle()
+        {
+            return cycle != null;
+        }
+
+        public Stack<object> getCycle()
+        {
+            return cycle;
+        } 
+    }
+}
---
+++ Assets/Source/GraphAlgorithm/8_DirectedCycle/Editor/testDirectedCycle.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Graph
+{
+    public class testDirectedCycle
+    {
+        private Digraph initDG()
+        {
+            Digraph g = new Digraph(13);
+            g.addEdge(4, 2);
+            g.addEdge(2, 3);
+            g.addEdge(3, 2);
+            g.addEdge(6, 0);
+            g.addEdge(0, 1);
+            g.addEdge(2, 0);
+            g.addEdge(11, 12);
+            g.addEdge(12, 9);
+            g.addEdge(9, 10);
+            g.addEdge(9, 11);
+            g.addEdge(8, 9);
+            g.addEdge(10, 12);
+            g.addEdge(11, 4);
+            g.addEdge(4, 3);
+            g.addEdge(3, 5);
+            g.addEdge(7, 8);
+            g.addEdge(8, 7);
+            g.addEdge(5, 4);
+            g.addEdge(0, 5);
+            g.addEdge(6, 4);
+            g.addEdge(6, 9);
+            g.addEdge(7, 6);
+            return g;
+        }
+
+        [Test]
+        public void DirectedCycle_hasCycle_true()
+        {
+            var g = initDG();
+            var dc = new DirectedCycle(g);
+            var res = dc.hasCycle();
+            Assert.True(res);
+        }
+    }
+}
+
