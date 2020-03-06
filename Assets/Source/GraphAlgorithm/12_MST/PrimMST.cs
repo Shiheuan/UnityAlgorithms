381ccd8369d6d1a99ffe057c6360c6763f0cd5ed
Shiheuan <shiheuan@gmail.com>
2020-03-06 23:16:12 +0800

    In time Prim MST.

---
+++ Assets/Source/GraphAlgorithm/12_MST/PrimMST.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using Algorithms.Sorting;
+
+namespace Algorithms.Graph
+{
+    public class PrimMST : IMST
+    {
+        private bool[] mark;
+        private Edge[] edgeTo;
+        private double[] distTo;
+        private IndexMinPQ<double> pq;
+
+        public PrimMST(EdgeWeightedGraph G)
+        {
+            edgeTo = new Edge[G.v()];
+            distTo = new double[G.v()];
+            mark = new bool[G.v()];
+            for (int v = 0; v < G.v(); v++)
+            {
+                distTo[v] = double.MaxValue;
+            }
+            pq = new IndexMinPQ<double>(4);
+
+            distTo[0] = 0.0;
+            pq.insert(0, 0.0);
+            while (!pq.isEmpty())
+            {
+                visit(G, pq.delMin());
+            }
+        }
+
+        private void visit(EdgeWeightedGraph G, int v)
+        {
+            mark[v] = true;
+            foreach (Edge e in G.adj(v))
+            {
+                int w = e.other(v);
+
+                if (mark[w]) continue;
+                if (e.getWeight() < distTo[w])
+                {
+                    edgeTo[w] = e;
+                    distTo[w] = e.getWeight();
+                    if (pq.contains(w)) pq.change(w, distTo[w]);
+                    else pq.insert(w, distTo[w]);
+                }
+            }
+        }
+        public IEnumerable edges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double weight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
