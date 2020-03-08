e5718f255722e475e46194a45752a14d3a517787
Shiheuan <shiheuan@gmail.com>
2020-03-08 18:11:31 +0800

    Kruskal MST.

---
+++ Assets/Source/GraphAlgorithm/12_MST/KruskalMST.cs
@@ -0,0 +1,41 @@
+﻿using Algorithms.Sorting;
+using Algorithms.Foundations;
+using System.Collections;
+
+namespace Algorithms.Graph
+{
+    public class KruskalMST : IMST
+    {
+        private Queue<Edge> mst;
+
+        public KruskalMST(EdgeWeightedGraph G)
+        {
+            mst = new Queue<Edge>();
+            MinPriorityQueue<Edge> pq = new MinPriorityQueue<Edge>();
+            foreach (Edge e in G.edges())
+            {
+                pq.insert(e);
+            }
+            UF uf = new UF(G.v());
+
+            while (!pq.isEmpty() && mst.size() < G.v() - 1)
+            {
+                Edge e = pq.delMin();
+                int v = e.either(), w = e.other(v);
+                if (uf.connected(v, w)) continue;
+                uf.union(v, w);
+                mst.enqueue(e);
+            }
+        }
+
+        public IEnumerable edges()
+        {
+            return mst;
+        }
+
+        public double weight()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
